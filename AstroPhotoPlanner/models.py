2affaff355a0099f0c3fdd6ef43262928186a818
Michal Dubovsky <michal.dubovsky@cern.ch>
2025-12-01 21:42:23 +0100

    Adding more code

--- AstroPhotoPlanner/models.py
+++ AstroPhotoPlanner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# user profile model to store user preferences
+class UserProfile(models.Model):
+    name = models.CharField(max_length=40)
+    gps_lat = models.FloatField()
+    gps_lon = models.FloatField()
+    astronomical_night_angle_limit = models.FloatField(default=-18.0)  # default to astronomical night
+    minimal_target_angle_above_horizon = models.FloatField(default=30.0)
--- AstroPhotoPlanner/views.py
+++ AstroPhotoPlanner/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from AstroPhotoPlanner.models import UserProfile
 
 # Create your views here.
 def index_page(request):
-    return render(request, 'AstroPhotoPlanner/index.html')
+    user_profiles = UserProfile.objects.all()
+    user_info = user_profiles[0] if user_profiles else None
+    return render(request, 'AstroPhotoPlanner/index.html', {'user_info': user_info})
