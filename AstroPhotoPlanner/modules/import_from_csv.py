d9c6a1ab47b3936c4f3a6f28271a67fa4941bbe2
Michal Dubovsky <michal.dubovsky@cern.ch>
2025-12-06 17:36:48 +0100

    Bootstrap classes for buttons

--- AstroPhotoPlanner/modules/import_from_csv.py
+++ AstroPhotoPlanner/modules/import_from_csv.py
@@ -13,6 +13,10 @@ def import_catalogue_from_csv(catalogue : Catalogue, csv_file_path : str) -> Non
         object_type = row.get('type')
         if pd.isna(object_type) or type(object_type) != str:
             row['type'] = ""
+        if pd.isna(row.get('plan_to_photograph')):
+            row['plan_to_photograph'] = True
+        else:
+            row['plan_to_photograph'] = bool(row['plan_to_photograph'])
         DeepSkyObject.objects.create(
             catalogue=catalogue,
             name=row['name'],
@@ -20,4 +24,12 @@ def import_catalogue_from_csv(catalogue : Catalogue, csv_file_path : str) -> Non
             dec=row['dec'],
             magnitude=row.get('magnitude', None),
             object_type=row.get('type', "").strip(),
+            plan_to_photograph=row['plan_to_photograph']
         )
+
+def export_catalogue_to_csv(catalogue : Catalogue) -> str:
+    deep_sky_objects = catalogue.deep_sky_objects.all()
+    result = "name,ra,dec,magnitude,type,plan_to_photograph\n"
+    for obj in deep_sky_objects:
+        result += f"{obj.name},{obj.ra},{obj.dec},{obj.magnitude if obj.magnitude is not None else ''},{obj.object_type if obj.object_type else ''},{obj.plan_to_photograph}\n"
+    return result
